6c93968718b07dfb06a1a00a28190571de2f7292
Bas Gerritsen <bas.gerritsen@pronkjuweel.nl>
2020-06-11 21:55:27 +0200

    Changed titles and finished checkout design.

--- src/app/components/web-deal/wd-prices/wd-prices.component.ts
+++ src/app/components/web-deal/wd-prices/wd-prices.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { WdCheckoutComponent } from '../wd-checkout/wd-checkout.component';
 
 @Component({
@@ -8,13 +8,14 @@ import { WdCheckoutComponent } from '../wd-checkout/wd-checkout.component';
   styleUrls: ['./wd-prices.component.scss'],
 })
 export class WdPricesComponent implements OnInit {
-
   constructor(private modalService: NgbModal) {}
 
-  open() {
-    const modalRef = this.modalService.open(WdCheckoutComponent, { size: 'xl' });
-    modalRef.componentInstance.deal = 'Webshop Plus Deal';
-  }
-
   ngOnInit() {}
+
+  openCheckout(deal: string) {
+    setTimeout(() => {
+      const modalRef = this.modalService.open(WdCheckoutComponent, { size: 'xl' });
+      modalRef.componentInstance.deal = deal;
+    }, 150);
+  }
 }
