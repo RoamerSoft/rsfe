b32f11eae6a345b3dc7212df9a2a776d4248b909
Bas Gerritsen <bas@bass-mbp.home>
2021-08-19 11:05:11 +0200

    Fixed routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
   },
-  { path: 'webdeals',
-  loadChildren: () => import('./pages/web-deal/web-deal.module').then(m => m.WebDealModule)
-},
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
