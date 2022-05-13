55e8a8053812c6c09b68d277d15279f9c0272952
bas.gerritsen <bas.gerritsen@pronkjuweel.nl>
2022-05-13 14:23:22 -0400

    Made an conversion mode for navbar.

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TranslationService } from '../../services/translation-service/translation.service';
-import { UtilsService } from '../../services/utils-service/utils.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,26 +8,17 @@ import { UtilsService } from '../../services/utils-service/utils.service';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  @Input() conversionMode: boolean;
   public currentLang: string;
-
-  public webDealPage = false;
-  public homePage = true;
 
   constructor(
     private translationService: TranslationService,
-    public route: ActivatedRoute,
-    private utilService: UtilsService
-  ) {}
+    public route: ActivatedRoute
+  ) {
+  }
 
   ngOnInit() {
     this.currentLang = this.translationService.getLang();
-
-    const lastPartOfUrl = this.utilService.getLastPartOfUrl(location.href);
-
-    if (lastPartOfUrl === 'webdeals') {
-      this.webDealPage = true;
-      this.homePage = false;
-    }
   }
 
   public switchLang(lang: string) {
