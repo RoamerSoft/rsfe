d6efed7163c6a8e3cbffce4cfb8aeae03665d04f
bas.gerritsen <bas.gerritsen@pronkjuweel.nl>
2022-08-30 15:45:25 +0100

    Finished maintenance packages and added payment urls.

--- src/app/pages/deal/components/maintenance/maintenance.component.ts
+++ src/app/pages/deal/components/maintenance/maintenance.component.ts
@@ -8,7 +8,6 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class MaintenanceComponent implements OnInit {
   @Input() showOrderButtons: boolean;
   @Output() buttonClicked = new EventEmitter<void>();
-  public showShadow: boolean;
 
   constructor() {
   }
