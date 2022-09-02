0530f03e7fb033bfe5c0dc24fd3b8125e9210463
bas.gerritsen <bas.gerritsen@pronkjuweel.nl>
2022-09-02 12:24:24 +0100

    Added time hack to loading popper.js/

--- src/app/pages/deal/deal.component.ts
+++ src/app/pages/deal/deal.component.ts
@@ -30,38 +30,6 @@ export class DealComponent implements OnInit {
 
   async ngOnInit() {
     await this.setTranslationAndMetaData();
-    this.loadScript();
-  }
-
-  public loadScript() {
-    let isFound = false;
-    const scripts = document.getElementsByTagName('script');
-    // tslint:disable-next-line: prefer-for-of
-    for (let i = 0; i < scripts.length; ++i) {
-      if (
-        scripts[i].getAttribute('src') != null &&
-        scripts[i].getAttribute('src').includes('loader')
-      ) {
-        isFound = true;
-      }
-    }
-
-    if (!isFound) {
-      const dynamicScripts = [
-        '/assets/js/popper.min.js',
-      ];
-
-      // tslint:disable-next-line: prefer-for-of
-      for (let i = 0; i < dynamicScripts.length; i++) {
-        const node = document.createElement('script');
-        node.src = dynamicScripts[i];
-        node.type = 'text/javascript';
-        node.async = false;
-        // tslint:disable-next-line: deprecation
-        node.charset = 'utf-8';
-        document.getElementsByTagName('head')[0].appendChild(node);
-      }
-    }
   }
 
   public async setTranslationAndMetaData() {
